77dd75422bb5710fdb4b8bd50206acf133f422d4
Vleis Walker <vleiswal@gmail.com>
2019-03-07 20:49:12 +0200

    Lecture 257 beginning

--- ToDoII/Controllers/TodoListViewController.swift
+++ ToDoII/Controllers/TodoListViewController.swift
@@ -49,6 +49,10 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
      
         //print(itemArray[indexPath.row])
+        
+//        context.delete(itemArray[indexPath.row])
+//        itemArray.remove(at: indexPath.row)
+        
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
         saveItems()
